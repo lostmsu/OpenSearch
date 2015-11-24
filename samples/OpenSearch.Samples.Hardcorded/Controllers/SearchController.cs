e3a5efe8f424e5e6c22b973e159ff88b00dd0ec6
Victor Milovanov <lostfreeman@gmail.com>
2015-11-23 22:57:45 -0800

    dummy solution

---
+++ samples/OpenSearch.Samples.Hardcorded/Controllers/SearchController.cs
@@ -0,0 +1,22 @@
+﻿namespace OpenSearch.Samples.Hardcoded.Controllers
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Threading.Tasks;
+	using Microsoft.AspNet.Mvc;
+
+	[Route("[controller]")]
+	public class SearchController : Controller
+	{
+		// GET
+		[HttpGet]
+		public IEnumerable<string> Get(
+			[FromQuery(Name = "q")] string query,
+			[FromQuery] string format,
+			[FromQuery(Name = "pw")] int? startPage)
+		{
+			return new string[] { "value1", "value2" };
+		}
+	}
+}
